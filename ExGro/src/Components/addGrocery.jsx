a1b63b0b766c89658ae421698330181185b6a3dd
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-05-13 15:38:58 -0400

    added date section, console log can give correct date but can't get it to work within an array. Still a WIP

--- ExGro/src/Components/addGrocery.jsx
+++ ExGro/src/Components/addGrocery.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react";
+import DatePicker from "./dateExp";
 
 export default function AddGroceryItem() {
   const [groceryItem, setGroceryItem] = useState("");
   const [updateList, setList] = useState([]);
+  const [expDate, setExpDate] = useState([]);
 
+  const expDateOutput = (event) => {
+    setExpDate(event.target.value);
+  };
   const groceryInput = (event) => {
     setGroceryItem(event.target.value);
   };
 
   const checkItem = () => {
     if (groceryItem.length > 0) {
+      setExpDate([...expDate, expDateOutput]);
       setList([...updateList, groceryItem]);
       setGroceryItem("");
+      console.log(updateList);
+      console.log(expDate);
     }
   };
 
   return (
     <section className="p-1 mr-10 border-2 rounded border-black">
-      <form className="grid grid-cols-2">
+      <form className="grid grid-cols-3">
         <div className="grid grid-rows-2">
           <h3>Grocery Item</h3>
           <input
@@ -34,13 +42,29 @@ export default function AddGroceryItem() {
                 className="outline-none border-2 border-black mt-1"
               >
                 {item}
+
               </li>
             ))}
           </ul>
         </div>
-        <button type="button" onClick={checkItem}>
-          Add
-        </button>
+        <div className="flex-start">
+          <h3>Expiration Date</h3>
+          <input
+            className="flex-center"
+            aria-label="Date"
+            type="date"
+            onChange={expDateOutput}
+          />
+        </div>
+        <div className="flex-start">
+          <button
+            className="outline-none border-2 border-black rounded p-2"
+            type="button"
+            onClick={checkItem}
+          >
+            Add to list
+          </button>
+        </div>
       </form>
     </section>
   );
